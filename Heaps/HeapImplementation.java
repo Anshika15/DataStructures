3279d72bb6ded83e8d2160f767b1a86a72f68c64
Anshika15 <bansalanshika20@gmail.com>
2021-03-20 16:32:02 +0530

    Heap insertion and deletion

--- Heaps/HeapImplementation.java
+++ Heaps/HeapImplementation.java
@@ -40,6 +40,51 @@ public class HeapImplementation {
 			}
 		}
 		
+		void pop()
+		{
+			// remove the top element
+			
+			// swap(list.get(1), list.get(last))
+			int lastIndex = list.size()-1;
+			int temp = list.get(1);
+			list.set(1, list.get(lastIndex));
+			list.set(lastIndex, temp);
+			
+			list.remove(lastIndex); // remove the last element
+			
+			heapify(1); // index of the node from which we want to start heapify
+		}
+		
+		void heapify(int index)
+		{
+			int left = 2*index;
+			int right = 2*index + 1;
+			
+			int minIndex = index;
+			// find the minimum element from child nodes as we have to swap with min elemeny.
+			//left < list.size() ==> element is in list and second cond. is to check minimum  element
+			if(left < list.size() && compare(list.get(left), list.get(index))) 
+			{
+				minIndex = left;
+			}
+			//right < list.size() ==> element is in list and second cond. is to check minimum  element
+			if(right < list.size() && compare(list.get(right), list.get(minIndex)))
+			{
+				minIndex = right;
+			}
+			
+			if(minIndex != index) // => we have to swap
+			{
+				// swap list[index] with list[minIndex];
+				
+				int temp = list.get(index);
+				list.set(index, list.get(minIndex));
+				list.set(minIndex, temp);
+				
+				heapify(minIndex);  // now heapify the swapped node.
+			}
+		}
+		
 		boolean empty()
 		{
 			return list.size() == 1;
@@ -57,8 +102,16 @@ public class HeapImplementation {
 		h.push(2);
 		h.push(20);
 		h.push(3);
-		System.out.println(h.top());
+		
 		System.out.println(h.list);
+		
+		while(!h.empty())
+		{
+			System.out.print(h.top() + " ");
+			h.pop();
+		}
+		
+		
 	}
 
 }
