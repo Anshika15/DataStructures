237ce180d46138ae48e04b5100d7248ebb0d809d
Anshika Bansal <54213105+Anshika15@users.noreply.github.com>
2021-08-10 11:11:28 +0530

    Target Sum Subsets

---
+++ Dynamic Programming/Dp-Revision/Target Sum Subsets.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int arr[] = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            arr[i] = s.nextInt();
+        }
+        
+        int target = s.nextInt();
+        
+        boolean dp[][] = new boolean[n+1][target+1];
+        
+        for(int i = 0; i < dp.length; i++)
+        {
+            for(int j = 0; j < dp[0].length; j++)
+            {
+                if(i == 0 && j == 0)
+                    dp[i][j] = true;
+                else if(i == 0)
+                    dp[i][j] = false;
+                else if(j == 0)
+                    dp[i][j] = true;
+                else
+                {
+                    if(dp[i-1][j] == true)
+                    {
+                        dp[i][j] = true;
+                    }
+                    else{
+                        int val = arr[i-1];
+                        if(j >= val)
+                        {
+                            if(dp[i-1][j-val] == true)
+                            {
+                                dp[i][j] = true;
+                            }
+                        }
+                    }
+                }
+                
+            }
+        }
+        
+        System.out.println(dp[arr.length][target]);
+
+    }
+}
