47ee3286dc1a79de320f64d1a4bfff72862a3c9b
Anshika Bansal <54213105+Anshika15@users.noreply.github.com>
2021-08-24 18:33:53 +0530

    Validate Bst

---
+++ Binary trees/Binary Tree - revision/Validate Bst.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class Main {
+    public static Scanner scn = new Scanner(System.in);
+
+    public static class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+    
+    public static TreeNode prev = null;
+
+    public static boolean isValidBST(TreeNode root) {
+        if(root == null)
+            return true;
+        if(!isValidBST(root.left))
+            return false;
+        if(prev != null && prev.val > root.val)
+            return false;
+        prev = root;
+        if(!isValidBST(root.right))
+            return false;
+        return true;
+    }
+
+    public static TreeNode createTree(int[] arr, int[] IDX) {
+        if (IDX[0] > arr.length || arr[IDX[0]] == -1){
+            IDX[0]++;
+            return null;
+        }
+
+        TreeNode node = new TreeNode(arr[IDX[0]++]);
+        node.left = createTree(arr, IDX);
+        node.right = createTree(arr, IDX);
+
+        return node;
+    }
+
+    public static void solve() {
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = scn.nextInt();
+
+        int[] IDX = new int[1];
+        TreeNode root = createTree(arr, IDX);
+        System.out.println(isValidBST(root));
+    }
+
+    public static void main(String[] args) {
+        solve();
+    }
+}
