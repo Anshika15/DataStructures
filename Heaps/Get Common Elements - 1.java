07d681f82b78ac22779e6897ed6c69238d9a05ae
Anshika Bansal <54213105+Anshika15@users.noreply.github.com>
2021-09-05 22:41:51 +0530

    Get Common Elements - 1

---
+++ Heaps/Get Common Elements - 1.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+public static void main(String[] args) throws Exception {
+    // write your code here
+    Scanner s = new Scanner(System.in);
+    
+    int n1 = s.nextInt();
+    int arr1[] = new int[n1];
+    for(int i = 0; i < n1; i++)
+        arr1[i] = s.nextInt();
+        
+    int n2 = s.nextInt();
+    int arr2[] = new int[n2];
+    for(int i = 0; i < n2; i++)
+        arr2[i] = s.nextInt();
+        
+    HashMap<Integer, Integer> map = new HashMap<>();
+    
+    for(int i : arr1)
+    {
+        if(map.containsKey(i))
+        {
+            map.put(i, map.get(i) + 1);
+        }
+        else
+        {
+            map.put(i, 1);
+        }
+    }
+    
+    for(int i : arr2)
+    {
+        if(map.containsKey(i)){
+            System.out.println(i);
+            map.remove(i);
+        }
+    }
+ }
+
+}
